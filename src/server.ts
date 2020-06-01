1805d3827310793aea9595cc156c8ff454b1140d
Gabriel Oliveira <biel--oliveira@hotmail.com>
2020-06-01 16:21:00 -0300

    Instalando dependencias iniciais, criando pastas e configurando minha primeira rota

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express, { response } from 'express';
+
+const app = express();
+
+app.get('/users', (request ,response) => {
+    console.log('Listagem de Usuários');
+    response.json([
+        'Diego',
+        'Cleiton',
+        'Robson',
+        'Gabriel'
+    ])
+});
+
+app.listen(3333);
